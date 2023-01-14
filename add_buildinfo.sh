e81e904dae13874161d45854eee76a6f62e8ba75
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2023-01-14 15:02:38 +0200

    Add add_buildinfo.sh script
    For adding the build info to index.html

---
+++ add_buildinfo.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+current_date=$(date --iso-8601=minutes)
+git_short_hash=$(git rev-parse --short HEAD)
+
+echo $current_date
+echo $git_short_hash
+
+sed -i "s/{0}/$current_date/g" wwwroot/index.html
+sed -i "s/{1}/$git_short_hash/g" wwwroot/index.html
